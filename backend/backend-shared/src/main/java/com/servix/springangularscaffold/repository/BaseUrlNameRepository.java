13f8ee28f4f4feb872788feb122c0f41e96255de
Cristian Chira <cristichira1@gmail.com>
2021-10-18 23:36:08 +0300

    add extension to JpaSpecificationExecutor in the BaseUrlNameRepository repo

--- backend/backend-shared/src/main/java/com/servix/springangularscaffold/repository/BaseUrlNameRepository.java
+++ backend/backend-shared/src/main/java/com/servix/springangularscaffold/repository/BaseUrlNameRepository.java
@@ -1,11 +1,12 @@
 package com.servix.springangularscaffold.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.NoRepositoryBean;
 
 import java.util.Optional;
 
 @NoRepositoryBean
-public interface BaseUrlNameRepository<T> extends JpaRepository<T, Long> {
+public interface BaseUrlNameRepository<T> extends JpaRepository<T, Long>, JpaSpecificationExecutor<T> {
     Optional<T> findByUrlName(String urlName);
 }
